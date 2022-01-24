49c68f6be3c24c5e3234e66ac1f35a53dc2a2c50
wujiepeng <wjp@xinlonghang.cn>
2022-01-24 23:36:22 +0800

    1

--- app/libs/redprint.py
+++ app/libs/redprint.py
@@ -21,6 +21,5 @@ class Redprint:
         if url_prefix is None:
             url_prefix = '/' + self.name
         for f, rule, options in self.mound:
-            endpoint = self.name + '+' + \
-                       options.pop("endpoint", f.__name__)
+            endpoint = self.name + '+' + options.pop("endpoint", f.__name__)
             bp.add_url_rule(url_prefix + rule, endpoint, f, **options)
